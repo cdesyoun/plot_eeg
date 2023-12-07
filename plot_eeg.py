2353d8fa0522cae015c7791cf081588528f9b789
Choonhan Youn <cdesyoun@users.noreply.github.com>
2023-12-06 17:32:01 -0800

    Update plot_eeg.py

--- plot_eeg.py
+++ plot_eeg.py
@@ -1,7 +1,7 @@
 import mne
 mne.viz.set_browser_backend('qt')
 
-datafile = '/Users/cyoun/plot_eeg_mne/sub-NDARAA948VFH_task-EC_run-1_eeg.set'
+datafile = './sub-NDARAA948VFH_task-EC_run-1_eeg.set'
 
 EEG = mne.io.read_raw_eeglab(datafile, preload=True)
 
