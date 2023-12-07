6ce2713bf6ee669966a3f8caf0c92d20747050cd
Choonhan Youn <cdesyoun@users.noreply.github.com>
2023-12-06 17:32:54 -0800

    Update plot_eeg.py

--- plot_eeg.py
+++ plot_eeg.py
@@ -1,11 +1,5 @@
 import mne
 mne.viz.set_browser_backend('qt')
-
-datafile = './sub-NDARAA948VFH_task-EC_run-1_eeg.set'
-
+datafile = './data/sub-NDARAA948VFH_task-EC_run-1_eeg.set'
 EEG = mne.io.read_raw_eeglab(datafile, preload=True)
-
-print("display start")
 EEG.filter(l_freq=1, h_freq=None).plot(block=True)
-print("display end")
-
